0c62ace25c91d754b26da79716fce7453b669809
Trevor <trevorh@onecell.co.za>
2019-10-28 06:51:49 +0200

    First Commit

---
+++ lpcxpresso55s69_hello_world_cm33_core0/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/Trevor\ Heyl/Documents/MCUXpressoIDE_11.0.1_2563/workspace/lpcxpresso55s69_hello_world_cm33_core1/Debug/lpcxpresso55s69_hello_world_cm33_core1.axf.o
+
+LIBS := -lpower_hardabi
+
---
+++ lpcxpresso55s69_hello_world_cm33_core1/Debug/drivers/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/fsl_clock.c \
+../drivers/fsl_common.c \
+../drivers/fsl_flexcomm.c \
+../drivers/fsl_gpio.c \
+../drivers/fsl_power.c \
+../drivers/fsl_reset.c \
+../drivers/fsl_usart.c 
+
+OBJS += \
+./drivers/fsl_clock.o \
+./drivers/fsl_common.o \
+./drivers/fsl_flexcomm.o \
+./drivers/fsl_gpio.o \
+./drivers/fsl_power.o \
+./drivers/fsl_reset.o \
+./drivers/fsl_usart.o 
+
+C_DEPS += \
+./drivers/fsl_clock.d \
+./drivers/fsl_common.d \
+./drivers/fsl_flexcomm.d \
+./drivers/fsl_gpio.d \
+./drivers/fsl_power.d \
+./drivers/fsl_reset.d \
+./drivers/fsl_usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/%.o: ../drivers/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -D__MULTICORE_NONE -DCPU_LPC55S69JBD100_cm33_core1 -DARM_MATH_CM33 -DMCMGR_HANDLE_EXCEPTIONS=1 -D__SEMIHOST_HARDFAULT_DISABLE=1 -DMULTICORE_APP=1 -DCPU_LPC55S69JBD100 -DCPU_LPC55S69JBD100_cm33_nodsp -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -D__MULTICORE_M33SLAVE -I../board -I../source -I../ -I../mcmgr -I../CMSIS -I../device -I../drivers -I../startup -I../component/lists -I../component/serial_manager -I../utilities -I../component/uart -O0 -fno-common -g3 -Wall -c  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin  -mcpu=cortex-m33+nodsp -mcpu=cortex-m33+nodsp -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
